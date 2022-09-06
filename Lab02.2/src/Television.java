daa4c6c27e34e890e31b08f1e0951bbec1ee2e7d
craigkelleher <kelleher.craig@gmail.com>
2022-09-06 10:58:43 -0700

    Added verifyInternetConnection. Reconfigured project, .idea files replaced.

--- Lab02.2/src/Television.java
+++ Lab02.2/src/Television.java
@@ -1,17 +1,42 @@
 public class Television {
+
   //every instance of class Television, will have the band and volume attributes
   //instance variables / fields
-  String brand; // brand name
-  int volume;  // current volume
+  private String brand; // brand name
+  private int volume;  // current volume
 
-  //method
-  void turnOn(){
+  public void turnOn(){
     // Concatenation, a single string that is a concatenation of The + brand + text + volume)
+    //invoke helper function. Verify internet question
+    boolean isConnected = verifyInternetConnection();
     System.out.println("The " + brand + " television is on, with a volume of " + volume);
   }
 
-  void turnOff(){
+  public void turnOff(){
     // Concatenation
     System.out.println("The " + brand + " television is off");
+  }
+
+  public String getBrand() {
+    return brand;
+  }
+
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
+
+  public int getVolume() {
+    return volume;
+  }
+
+  public void setVolume(int volume) {
+    this.volume = volume;
+  }
+
+  private boolean verifyInternetConnection(){
+    return true;
+  }
+  public String toString() {
+    return "Television{" + "brand=" + getBrand() + ", volume=" + getVolume() + "}";
   }
 }
